7a069869857aac3ee8481f0c7e3e84aafc2c01ef
Aleksey <70802793+WolfishGraf@users.noreply.github.com>
2020-11-01 17:31:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace Практика4._2
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue("First");
+            queue.Enqueue("Second");
+            queue.Enqueue("Third");
+            queue.Enqueue("Fourth");
+            while (queue.Count > 0)
+            {
+                object obj = queue.Dequeue();
+                Console.WriteLine("From Queue:{0}", obj);
+            }
+
+            Stack stack = new Stack();
+            stack.Push("First");
+            stack.Push("Second");
+            stack.Push("Third");
+            stack.Push("Fourth");
+            while (stack.Count > 0)
+            {
+                object obj = stack.Pop();
+                Console.WriteLine("'From Stack: {0}", obj);
+            }
+        }
+    }
+
+   
+}
